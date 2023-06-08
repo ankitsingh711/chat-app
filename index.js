ddc075b478134dbd8633c38f0137dde64b04d2b4
ankitsingh711 <developerankit2127@gmail.com>
2023-06-08 14:57:07 +0530

    done

--- index.js
+++ index.js
@@ -3,24 +3,9 @@ const { connection } = require("./config/db");
 const app = express();
 require("dotenv").config();
 const { UserRouter } = require("./router/UserRouter");
-const { Server } = require("socket.io");
-const http = require("http");
-const server = http.createServer(app);
-const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname, "/chat.html");
-});
-
-io.on("connection", (socket) => {
-  console.log("A user connected", socket.id);
-  io.emit("message", (message) => {
-    console.log(message);
-  });
-});
-
-io.on("error", (err) => {
-  console.log(err);
 });
 
 app.use(express.json());
